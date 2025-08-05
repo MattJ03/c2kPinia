ee5ea1f3d5c20fdcbd5528adff2a0914e99287ee
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-05 23:26:52 +0100

    fixed spelling issue on route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
-Router::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('api:sanctum')->group(function () {
     Route::get('/student/index', [StudentController::class, 'index']);
     Route::post('/student/', [StudentController::class, 'store']);

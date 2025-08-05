268217d89783572b6a2455172cae81a4ba777f4f
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-05 23:11:30 +0100

    usercontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $validatedData = $request->validate([
+          'email' => 'required|email|unique:users|max:20',
+          'password' => 'required|max:20|password',
+        ]);
+
+        $user = User::create([
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+        return response()->json(['message' => 'user created']);
+    }
+
+    public function login(Request $request) {
+        $validatedData = $request->validate([
+           'email' => 'required|email|unique:users|max:20',
+           'password' => 'required|max:20|password',        
+        ]);
+
+        if(!Auth::attempt($validatedData)) {
+            return response()->json(['message' => 'invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+}
+
 }

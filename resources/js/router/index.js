6ff03a3e467abc34154f160f3d5dbad6ccb56b8a
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-06 22:24:30 +0100

    created and configured router

---
+++ resources/js/router/index.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue;'
+import { createRouter } from 'vue-router';
+import { createWebHistory } from 'vue-router';
+
+const routes = [
+    {}
+];
+
+const router = creteRouter({
+    routes, 
+    history: createWebHistory();
+});

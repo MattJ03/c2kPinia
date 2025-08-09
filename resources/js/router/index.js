37c908ad95453618cdfe1fe206e2714c3efa7ecb
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-10 00:51:17 +0100

    added create to router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue;'
 import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
+import create from '../pages/create.vue';
 
 const routes = [
-    {}
+    { path: '/create', component: create },
+    
 ];
 
 const router = createRouter({

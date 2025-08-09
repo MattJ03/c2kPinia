5b95fc5aa71c6aed140ce38e8ad6b5645ea3559e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-08-09 18:41:55 +0100

    majority of create form finishd logic wise

---
+++ resources/js/pages/create.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="form-container">
+    <h1 class="header-create"> Create Form </h1>
+    <div class="form-group">
+        <label> First Name </label>
+        <input type="text" v-model="form.first_name" required />
+    </div>
+    <div class="form-group">
+        <label> Last Name </label>
+        <input type="text" v-model="form.last_name" required />
+    </div>
+    <div class="form-group">
+        <label> Email </label>
+        <input type="email" v-model="form.email" required/>
+    </div>
+    <div class="form-group" />
+    <label> Phone Number </label>
+    <input type="number" v-model="form.phone" required />
+    </div>
+    <
+</div>
+</template>
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+import router from '../router/index';
+import StudentStore, { useStudentStore } from '../store/studentStore.vue'
+
+const store = useStudentStore();
+
+const form = reactive({
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone: '',
+    gender: '',
+});
+
+
+
+</script>
+<style scoped>
+.header-create {
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+.form-container {
+    margin: 2px;
+    border: 8px;
+    border-radius: 14px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: blue;
+    color: white;
+}
+.form-group {
+    margin-bottom: 2px;
+    border-radius: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
